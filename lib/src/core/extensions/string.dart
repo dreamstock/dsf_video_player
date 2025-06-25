a0603a4750b4db8157f7767e3618eebe4dc4703f
Shotaro Yamazaki <shotaro.ibramessi@gmail.com>
2025-06-25 17:49:16 +0900

    add translation title function

---
+++ lib/src/core/extensions/string.dart
@@ -0,0 +1,283 @@
+extension StringExt on String {
+  String toTranslateVideoTitle() {
+    switch (this) {
+      case 'Aerial Duel':
+        return 'エアリアルデュエル';
+      case 'Hand Pass':
+        return 'ハンドパス';
+      case 'Long Pass':
+        return 'ロングパス';
+      case 'Pass to Final Third':
+        return 'ファイナルサードへのパス';
+      case 'Penalty Save':
+        return 'PKセーブ';
+      case 'Progressive Pass':
+        return '前進パス';
+      case 'Progressive Long Pass':
+        return '前進ロングパス';
+      case 'Save':
+        return 'セーブ';
+      case 'Interception':
+        return 'インターセプト';
+      case 'Long Pass to Final Third':
+        return 'ロングパス（ファイナルサード）';
+      case 'Defensive Duel':
+        return 'ディフェンシブデュエル';
+      case 'Loose Ball Duel':
+        return 'ルーズボールデュエル';
+      case 'Progressive Run':
+        return '前方へのドリブル';
+      case 'Shot Block':
+        return 'シュートブロック';
+      case 'Clearance':
+        return 'クリアランス';
+      case 'Long Progressive Pass':
+        return 'ロング前進パス';
+      case 'Long Progressive Pass to Final Third':
+        return 'ロング前方へのパス（ファイナルサード）';
+      case 'Long Progressive Pass to Penalty Area':
+        return 'ロング前方へのパス（PA）';
+      case 'Long Pass to Penalty Area':
+        return 'ロングパス（PA）';
+      case 'Progressive Pass to Final Third':
+        return '前方へのパス（ファイナルサード）';
+      case 'Progressive Pass to Penalty Area':
+        return '前方へのパス（PA）';
+      case 'Pass to Penalty Area':
+        return 'PAへのパス';
+      case 'Shot on Target':
+        return '枠内シュート';
+      case 'Goal':
+        return 'ゴール';
+      case 'Assist':
+        return 'アシスト';
+      case 'Cross from Inside the Penalty Area':
+        return 'PA内からのクロス';
+      case 'Cross next to the Penalty Area':
+        return 'PA脇からのクロス';
+      case 'Cross from before the Penalty Area':
+        return 'PA手前からのクロス';
+      case 'Dribble':
+        return 'ドリブル';
+      case 'Free Kick Cross':
+        return 'フリーキッククロス';
+      case 'Offensive Duel':
+        return 'オフェンシブデュエル';
+      case 'Shot':
+        return 'シュート';
+      case 'Long Smart Progressive Pass':
+        return 'ロングスマート前進パス';
+      case 'Long Key Progressive Pass':
+        return 'ロングキーパス（前進）';
+      case 'Long Key Progressive Pass to Final Third':
+        return 'ロングキーパス（ファイナルサード）';
+      case 'Long Smart Progressive Pass to Final Third':
+        return 'ロングスマートパス（ファイナルサード）';
+      case 'Long Key Progressive Pass to Penalty Area':
+        return 'ロングキーパス（PA）';
+      case 'Long Smart Progressive Pass to Penalty Area':
+        return 'ロングスマートパス（PA）';
+      case 'Long Key Pass':
+        return 'ロングキーパス';
+      case 'Long Smart Pass':
+        return 'ロングスマートパス';
+      case 'Long Key Pass to Final Third':
+        return 'ロングキーパス（ファイナルサード）';
+      case 'Long Smart Pass to Final Third':
+        return 'ロングスマートパス（ファイナルサード）';
+      case 'Long Key Pass to Penalty Area':
+        return 'ロングキーパス（PA）';
+      case 'Long Smart Pass to Penalty Area':
+        return 'ロングスマートパス（PA）';
+      case 'Progressive Key Pass':
+        return '前方へのキーパス';
+      case 'Progressive Smart Pass':
+        return '前方へのスマートパス';
+      case 'Progressive Key Pass to Final Third':
+        return '前方へのキーパス（ファイナルサード）';
+      case 'Progressive Smart Pass to Final Third':
+        return '前方へのスマートパス（ファイナルサード）';
+      case 'Progressive Key Pass to Penalty Area':
+        return '前方へのキーパス（PA）';
+      case 'Progressive Smart Pass to Penalty Area':
+        return '前方へのスマートパス（PA）';
+      case 'Key Pass to Final Third':
+        return 'キーパス（ファイナルサード）';
+      case 'Smart Pass to Final Third':
+        return 'スマートパス（ファイナルサード）';
+      case 'Key Pass to Penalty Area':
+        return 'キーパス（PA）';
+      case 'Smart Pass to Penalty Area':
+        return 'スマートパス（PA）';
+      case 'Defensive Aerial Duel':
+        return '守備時エアリアルデュエル';
+      case 'Offensive Aerial Duel':
+        return '攻撃時エアリアルデュエル';
+      case 'Key Short/Medium Pass':
+        return 'キーミドルパス';
+      case 'Smart Long Pass to Final Third':
+        return 'スマートロングパス（ファイナルサード）';
+      case 'Smart Long Pass to Penalty Area':
+        return 'スマートロングパス（PA）';
+      case 'Smart Long Pass':
+        return 'スマートロングパス';
+      case 'Smart Short/Medium Pass':
+        return 'スマートミドルパス';
+      case 'Smart Short/Medium Pass to Penalty Area':
+        return 'スマートミドルパス（PA）';
+      case 'Smart Short/Medium Pass to Final Third':
+        return 'スマートミドルパス（ファイナルサード）';
+      case 'Key Long Pass':
+        return 'キーロングパス';
+      case 'Key Long Pass to Final Third':
+        return 'キーロングパス（ファイナルサード）';
+      case 'Key Short/Medium Pass to Final Third':
+        return 'キーミドルパス（ファイナルサード）';
+      case 'Key Long Pass to Penalty Area':
+        return 'キーロングパス（PA）';
+      case 'Key Short/Medium Pass to Penalty Area':
+        return 'キーミドルパス（PA）';
+      case 'Short/Medium Pass to Final Third':
+        return 'ミドルパス（ファイナルサード）';
+      case 'Short/Medium Pass to Penalty Area':
+        return 'ミドルパス（PA）';
+      case 'Smart Cross to Penalty Area (Shot Assist)':
+        return 'スマートクロス（PA・アシスト）';
+      case 'Smart Cross to Penalty Area':
+        return 'スマートクロス（PA）';
+      case 'Key Cross to Penalty Area (Shot Assist)':
+        return 'キークロス（PA・アシスト）';
+      case 'Key Cross to Penalty Area':
+        return 'キークロス（PA）';
+      case 'Cross to Penalty Area (Shot Assist)':
+        return 'クロス（PA・アシスト）';
+      case 'Cross to Penalty Area':
+        return 'クロス（PA）';
+      case 'Cross to Outside the Penalty Area':
+        return 'クロス（PA外）';
+      case 'Acceleration':
+        return '加速';
+      case 'Shot Off Target':
+        return '枠外シュート';
+      case 'Key Pass':
+        return 'キーパス';
+      case 'Shot Assist':
+        return 'シュートアシスト';
+      case 'Smart Pass to Final Third (Shot Assist)':
+        return 'スマートパス（ファイナルサード・アシスト）';
+      case 'Smart Pass to Penalty Area (Shot Assist)':
+        return 'スマートパス（PA・アシスト）';
+      case 'Smart Pass (Shot Assist)':
+        return 'スマートパス（アシスト）';
+      case 'Smart Pass':
+        return 'スマートパス';
+      case 'Key Pass (Shot Assist)':
+        return 'キーパス（アシスト）';
+      case 'Key Pass to Final Third (Shot Assist)':
+        return 'キーパス（ファイナルサード・アシスト）';
+      case 'Key Pass to Penalty Area (Shot Assist)':
+        return 'キーパス（PA・アシスト）';
+      case 'Pass to Final Third (Shot Assist)':
+        return 'ファイナルサードへのパス（アシスト）';
+      case 'Pass to Penalty Area (Shot Assist)':
+        return 'PAへのパス（アシスト）';
+      case 'Lost Aerial Duel':
+        return 'エアリアルデュエル敗北';
+      case 'Penalty Conceded Goal':
+        return 'PK失点';
+      case 'Conceded Goal':
+        return '失点';
+      case 'Innaccurate Goal Kick':
+        return 'ゴールキックミス';
+      case 'Foul':
+        return 'ファウル';
+      case 'Penalty Foul':
+        return 'PKファウル';
+      case 'Red Card':
+        return 'レッドカード';
+      case 'Yellow Card':
+        return 'イエローカード';
+      case 'Innaccurate Short/Medium Pass to Final Third':
+        return 'ミドルパスミス（ファイナルサード）';
+      case 'Innaccurate Long Pass to Final Third':
+        return 'ロングパスミス（ファイナルサード）';
+      case 'Innaccurate Pass to Final Third':
+        return 'ファイナルサードへのパスミス';
+      case 'Innaccurate Pass to Penalty Area':
+        return 'PAへのパスミス';
+      case 'Innaccurate Long Pass to Penalty Area':
+        return 'ロングパスミス（PA）';
+      case 'Own Half Innaccurate Short/Medium Pass':
+        return '自陣ミドルパスミス';
+      case 'Own Half Innaccurate Long Pass':
+        return '自陣ロングパスミス';
+      case 'Own Half Innaccurate Pass':
+        return '自陣パスミス';
+      case 'Innaccurate Short/Medium Pass':
+        return 'ミドルパスミス';
+      case 'Innaccurate Long Pass':
+        return 'ロングパスミス';
+      case 'Innaccurate Pass':
+        return 'パスミス';
+      case 'Innaccurate Short/Medium Hand Pass to Final Third':
+        return 'ハンドパスミス（ファイナルサード）';
+      case 'Innaccurate Long Hand Pass to Final Third':
+        return 'ロングハンドパスミス（ファイナルサード）';
+      case 'Innaccurate Hand Pass to Final Third':
+        return 'ファイナルサードへのハンドパスミス';
+      case 'Own Half Innaccurate Short/Medium Hand Pass':
+        return '自陣ハンドパスミス（ミドル）';
+      case 'Own Half Innaccurate Long Hand Pass':
+        return '自陣ハンドパスミス（ロング）';
+      case 'Own Half Innaccurate Hand Pass':
+        return '自陣ハンドパスミス';
+      case 'Innaccurate Short/Medium Hand Pass':
+        return 'ミドルハンドパスミス';
+      case 'Innaccurate Long Hand Pass':
+        return 'ロングハンドパスミス';
+      case 'Innaccurate Hand Pass':
+        return 'ハンドパスミス';
+      case 'Innaccurate Short/Medium Pass to Penalty Area':
+        return 'ミドルパスミス（PA）';
+      case 'Unsuccessful Defensive Duel (Taken on)':
+        return '対人守備失敗（抜かれた）';
+      case 'Unsuccessful Defensive Duel':
+        return '対人守備失敗';
+      case 'Unsuccessful Sliding Tackle':
+        return 'スライディング失敗';
+      case 'Unsuccessful Sliding Tackle (Taken on)':
+        return 'スライディング失敗（抜かれた）';
+      case 'Missed Opportunity (On Target)':
+        return '決定機ミス（枠内）';
+      case 'Missed Opportunity (Off Target)':
+        return '決定機ミス（枠外）';
+      case 'Innaccurate Free Kick Shot':
+        return 'FKシュートミス';
+      case 'Penalty Miss':
+        return 'PK失敗';
+      case 'Own Half Unsuccessful Dribble':
+        return '自陣ドリブル失敗';
+      case 'Own Half Unsuccessful Offensive Duel':
+        return '自陣攻撃デュエル失敗';
+      case 'Lost Offensive Aerial Duel':
+        return '攻撃エアリアルデュエル敗北';
+      case 'Lost Defensive Aerial Duel':
+        return '守備エアリアルデュエル敗北';
+      case 'Innaccurate Cross to Penalty Area':
+        return 'PAへのクロスミス';
+      case 'Innaccurate Cross':
+        return 'クロスミス';
+      case 'Blocked Cross':
+        return 'クロスブロック';
+      case 'Unsuccessful Corner':
+        return 'コーナー失敗';
+      case 'Unsuccessful Free Kick Cross':
+        return 'FKクロス失敗';
+      case 'Unsuccessful Dribble':
+        return 'ドリブル失敗';
+      case 'Unsuccessful Offensive Duel':
+        return '攻撃デュエル失敗';
+    }
+    return this;
+  }
+}
