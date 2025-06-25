0635abab06c239ae5d8672e3530da2b60b12e88a
Shotaro Yamazaki <shotaro.ibramessi@gmail.com>
2025-06-25 17:52:49 +0900

    add translation title function

--- lib/dsf_video_player.dart
+++ lib/dsf_video_player.dart
@@ -1,3 +1,4 @@
+export 'src/core/extensions/string.dart';
 export 'src/models/videos_entry_payload.dart';
 export 'src/ui/dsf_playlist_player.dart' show DsfPlaylistPlayer;
 export 'src/ui/video_select_components/video_display_tile.dart'
